5cfc12a3512fee97d82fb2670d261d546a6d02dc
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 19:48:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,6 +31,10 @@ def adding
   }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest[muskets]= 2
+return "whale bone corsets" => 5
+"porcelain vases" => 2
+"oil paintings" => 3
+"muskets" => 2
 
 
 
