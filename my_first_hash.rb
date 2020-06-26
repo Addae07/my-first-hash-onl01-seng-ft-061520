66249f84b99345718a78abf1857ea12a0b20743a
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 19:57:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,9 +14,9 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale bone corsets" => "5", 
+                  "porcelain vases" => "2", 
+                  "oil paintings" => "3"
                  }
  shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
@@ -25,8 +25,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => "5", 
+                  "porcelain vases" => "2", 
                   "oil paintings" => "3"
   }
   # add 2 muskets to the shipping_manifest hash below
